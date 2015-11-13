fb69516a4ca38be0cebcf33c03147788c744f53a
Parashuram <code@nparashuram.com>
2015-11-12 22:10:04 -0800

    Pick up deps for node 5 too
    fixes #5

--- cli/android-cli.js
+++ cli/android-cli.js
@@ -47,23 +47,25 @@ function writeIfNotExists(filename, data) {
 
 Android.prototype.add = function(plugin) {
     this.init();
+    var self = this;
     return cordova.plugman.raw.install('android', PLATFORM_DIR, plugin, path.resolve(this.projectRoot, 'node_modules'), {
         platformVersion: '4.0.0',
         //TODO - figure out a way to make cordova browserify only to selectively pick files
         browserify: false
     }).then(function() {
-        return generateCordovaJs();
+        return generateCordovaJs(self.projectRoot);
     }).then(function() {
         console.log('Plugin %s added for Android', plugin);
     });
 };
 
 Android.prototype.remove = function(plugin) {
+    var self = this;
     return cordova.plugman.raw.uninstall('android', PLATFORM_DIR, plugin, path.resolve(this.projectRoot, 'node_modules'), {
         platformVersion: '4.0.0',
         browserify: false
     }).then(function() {
-        return generateCordovaJs();
+        return generateCordovaJs(self.projectRoot);
     }).then(function() {
         console.log('Plugin %s removed for Android', plugin);
     });
@@ -72,30 +74,40 @@ Android.prototype.remove = function(plugin) {
 Android.prototype.clean = function() {
     var projectRoot = this.projectRoot;
     return Q().then(function() {
+        rimraf.sync(PLATFORM_DIR);
+        mkdirp.sync(path.resolve(PLATFORM_DIR));
         fs.writeFileSync(path.resolve(PLATFORM_DIR, 'AndroidManifest.xml'), ANDROID_MANIFEST);
+
         rimraf.sync(path.resolve(projectRoot, 'node_modules/android.json'));
         rimraf.sync(path.resolve(projectRoot, 'node_modules/fetch.json'));
-        rimraf.sync(path.resolve(PLATFORM_DIR, 'src'));
-        rimraf.sync(path.resolve(PLATFORM_DIR, 'res'));
-        rimraf.sync(path.resolve(PLATFORM_DIR, 'libs'));
-        rimraf.sync(path.resolve(PLATFORM_DIR, 'assets'));
+        rimraf.sync(path.resolve(projectRoot, 'node_modules/android'))
         console.log('If you still have trouble adding/removing plugins, delete all the plugin from node_modules');
     });
 };
 
 Android.prototype.repair = function() {
+    var self = this;
     return Q().then(function() {
-        return generateCordovaJs();
+        return generateCordovaJs(self.projectRoot);
     });
 };
 
-function generateCordovaJs() {
-    var CORDOVA_JS = path.resolve(__dirname, '../node_modules/cordova-lib/node_modules/cordova-js/src');
-
+function generateCordovaJs(projectRoot) {
+    var cordovaJSNpm = glob.sync('**/cordova-js', {
+        cwd: path.resolve(projectRoot, 'node_modules'),
+        realpath: true
+    });
+    var CORDOVA_JS = '';
+    if (cordovaJSNpm.length !== 1) {
+        console.log('Could not find Cordova-JS node mobule');
+        return;
+    } else {
+        CORDOVA_JS = path.join(cordovaJSNpm[0], 'src');
+    }
     // TODO - Use a stream instead of reading all content to buffer
     var requireJSContent = fs.readFileSync(path.resolve(CORDOVA_JS, 'scripts/require.js'), 'utf-8');
     var pluginsContent = glob.sync('**/*.js', {
-        cwd: path.resolve(PLATFORM_DIR, 'assets/www'),
+        cwd: path.resolve(PLATFORM_DIR, 'platform_www'),
         realpath: true
     }).map(function(filename) {
         return fs.readFileSync(filename, 'utf-8');
@@ -113,7 +125,7 @@ function generateCordovaJs() {
         .replace('//<{{__PLUGINS__}}>', pluginsContent)
 
 
-    fs.writeFileSync(path.join(PLATFORM_DIR, 'assets/cordova.js'), template);
+    fs.writeFileSync(path.join(PLATFORM_DIR, 'assets/www/cordova.js'), template);
     console.log('Generated cordova.js');
 };
 
